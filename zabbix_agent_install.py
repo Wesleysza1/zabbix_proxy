7d7ed173a619d99f407725b69dda0bf7c4ff8146
wesley souza <wesley.souza@atos.net>
2022-08-02 10:45:37 -0300

    added python selector

---
+++ zabbix_agent_install.py
@@ -0,0 +1,20 @@
+import subprocess
+
+system = str(subprocess.check_output("cat /etc/*-release | grep PRETTY", shell=True))
+so = system.lower()
+print(so)
+
+if "ubuntu" in so:
+    subprocess.call(["ansible-playbook", "ubuntu-zabbix-proxy.yaml", "-K"])
+elif "debian" in so:
+    subprocess.call(["ansible-playbook", "debian-zabbix-proxy.yaml", "-K"])
+elif "redhat" in so: 
+    subprocess.call(["ansible-playbook", "rhel-zabbix-proxy.yaml", "-K"])
+elif "red hat" in so: 
+    subprocess.call(["ansible-playbook", "rhel-zabbix-proxy.yaml", "-K"])
+elif "centos" in so:
+    subprocess.call(["ansible-playbook", "centos-zabbix-proxy.yaml", "-K"])
+elif "suse" in so:
+    subprocess.call(["ansible-playbook", "suse-zabbix-proxy.yaml", "-K"])
+else:
+    ("SO non supported")
